f95188e1145fd58342a553967475c091b38b8af6
Alex Cameron <ascottcameron@gmail.com>
2020-01-18 17:07:13 +1100

    Implement tree search.

--- chap1/main.c
+++ chap1/main.c
@@ -2,6 +2,7 @@
 #include "slp.h"
 #include "prog1.h"
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -144,11 +145,65 @@ int maxargs(A_stm stm) {
   }
 }
 
+typedef struct tree *T_tree;
+struct tree {
+  T_tree left;
+  string key;
+  T_tree right;
+};
+
+T_tree Tree(T_tree l, string k, T_tree r) {
+  T_tree t = checked_malloc(sizeof(*t));
+  t->left = l;
+  t->key = k;
+  t->right = r;
+  return t;
+}
+
+T_tree insert(string key, T_tree t) {
+  if (t == NULL)
+    return Tree(NULL, key, NULL);
+  else if (strcmp(key, t->key) < 0)
+    return Tree(insert(key, t->left), t->key, t->right);
+  else if (strcmp(key, t->key) > 0)
+    return Tree(t->left, t->key, insert(key, t->right));
+  else
+    return Tree(t->left, key, t->right);
+}
+
+T_tree createTree() {
+  return Tree(Tree(Tree(NULL, "aa", NULL), "ab", NULL), "bb",
+              Tree(NULL, "bc", Tree(NULL, "zz", NULL)));
+}
+
+bool member(string key, T_tree t) {
+  if (!t)
+    return false;
+  int cmp = strcmp(key, t->key);
+  if (cmp > 0)
+    return member(key, t->right);
+  else if (cmp < 0)
+    return member(key, t->left);
+  else
+    return true;
+}
+
 int main() {
   A_stm slpProgram = prog();
   printf("=== maxargs ===\n");
   printf("The max number of print args is %d.\n", maxargs(slpProgram));
   printf("=== interp ===\n");
   interp(slpProgram);
+  printf("=== tree ===\n");
+  T_tree tree = createTree();
+  assert(member("aa", tree));
+  assert(member("ab", tree));
+  assert(member("bb", tree));
+  assert(member("bc", tree));
+  assert(member("zz", tree));
+  assert(!member("za", tree));
+  assert(!member("az", tree));
+  assert(!member("cb", tree));
+  assert(!member("az", tree));
   return 0;
 }
