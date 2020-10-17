b8fd964dafef2300907809fd8e9434f11a91a078
Alex Cameron <ascottcameron@gmail.com>
2020-10-17 23:26:52 +1100

    Print the IR tree.

--- chap7/parsetest.c
+++ chap7/parsetest.c
@@ -9,6 +9,7 @@
 #include "tree.h"
 #include "frame.h"
 #include "semant.h"
+#include "printtree.h"
 
 extern int yyparse(void);
 extern int yydebug;
@@ -23,7 +24,19 @@ void parse(string fname) {
   fprintf(stdout, "Abstract Syntax Tree:\n");
   if (absyn_root) {
     pr_exp(stdout, absyn_root, 0);
-    SEM_transProg(absyn_root);
+    F_fragList frags = SEM_transProg(absyn_root);
+    assert(frags);
+    fprintf(stdout, "\nIR Tree:\n");
+    while (frags) {
+      F_frag f = frags->head;
+      if (f->kind == F_procFrag) {
+        T_stm body = f->u.proc.body;
+        // Am I missing something here?
+        // Why are we expecting a T_stmList?
+        printStmList(stdout, T_StmList(body, NULL));
+      }
+      frags = frags->tail;
+    }
   }
 }
 
